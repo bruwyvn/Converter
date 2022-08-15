334c8435aa38b876ed362fba7c83c19548cc5585
BRUNO ANDRADE <09201292937@educacional.up>
2022-08-15 20:53:29 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Converter
+{
+    class Program
+    {
+        static int ConvertToInt(string stdin) {
+            int power = stdin.Length - 1;
+            int sum = 0;
+            foreach (var num in stdin)
+            {
+                sum = sum + ((num - 48) * (int) Math.Pow(10, power));
+                power--;
+            }
+            return sum;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um numero: ");
+            Console.WriteLine(ConvertToInt(Console.ReadLine()));
+        }
+    }
+}
